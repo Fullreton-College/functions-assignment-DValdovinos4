dcf7b6dc43a571b15dc5dfc29e3b2cef2f146250
DValdovinos4 <domanikvaldovinos@gmail.com>
2026-03-09 15:20:44 -0700

    Update Function_1.cpp

--- Function_1.cpp
+++ Function_1.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <string>
 #include <cstdlib>
 using namespace std;
 
-/* Define your function here */ 
+string CoinFlip()
+{
+    if (rand() %2==1){
+        return "Heads";
+    }
+    else
+    {
+         return "Tails";
+    }
+}
 
-int main() {
-   // Add more variables as needed
-   
-   srand(2);  // Unique seed
+int main() 
+{ 
+int numFlips;
 
-   /* Type your code here */
+srand(2);
 
-   return 0;
+    if(!(cin>>numFlips)) return 0;
+    
+    for(int i=0;i<numFlips;i++)
+    {
+        cout<<CoinFlip()<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
